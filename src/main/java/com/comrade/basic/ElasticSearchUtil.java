54d45df56b7c326676e2c81efd6d80e2bec5cafb
shiva508 <dasarishiva1@gmail.com>
2024-07-24 09:20:55 +0530

    Kafka streams usage

--- src/main/java/com/comrade/basic/ElasticSearchUtil.java
+++
@@ -1,9 +0,0 @@
-package com.comrade.basic;
-
-public class ElasticSearchUtil {
-	public static final String GROUP_ID="kafaka_elastic_search_application";
-	public static final String BOOTSTRAP_SERVER="127.0.0.1:9092";
-	public static final String TOPICS[]= {"twitter_tweets"};
-	public static final String EXTRACT_JSON_VALUE="id_str";
-	
-}
